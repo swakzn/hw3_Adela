44894b199bb145f6ea24aebaf154a256400db2c2
swakzn <byunm58@gmail.com>
2023-08-19 00:50:40 +0600

    dz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: '',
+            tasksList: [],
+        };
+    }
+
+    addTask = () => {
+        const { task, tasksList } = this.state;
+        if (task.trim() !== '') {
+            this.setState({
+                tasksList: [...tasksList, task],
+                task: '',
+            });
+        }
+    };
+
+    render() {
+        const { task, tasksList } = this.state;
+
+        return (
+            <div className="Todo-list">
+                <div className="input">
+                    <h1>HW 3</h1>
+                    <input
+                        type="text"
+                        placeholder="New to do"
+                        value={task}
+                        onChange={(e) => this.setState({ task: e.target.value })}
+                    />
+                    <button onClick={this.addTask}>Add</button>
+                </div>
+                <div className="list">
+                    {tasksList.map((task, index) => (
+                        <div key={index}>{task}</div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Todo;
